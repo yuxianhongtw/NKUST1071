5b7557a30637ef58e0dffa84a653eccd276fd877
鈺弦 洪 <yuxianhongtw@gmail.com>
2018-11-02 11:27:00 +0800

    add 1102's test

---
+++ 1102/OpenDataImport/OpenDataImport/Database/OpenDataDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenDataImport.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenDataImport.Database
+{
+    public class OpenDataDbContext: DbContext
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\[高科大]軟體工程\github\1102\OpenDataImport\OpenDataImport\App_Data\Database.mdf;Integrated Security=True";
+            }
+        }
+
+        public DbSet<OpenData> OpenData { get; set; }
+
+        public OpenDataDbContext() : base()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ 1102/OpenDataImport/OpenDataImport/Service/EFImportService.cs
@@ -0,0 +1,22 @@
+﻿using OpenDataImport.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenDataImport.Service
+{
+    class EFImportService
+    {
+        private Repository.EFOpenDataRepository _efRepository;
+
+        public EFImportService()
+        {
+            _efRepository = new Repository.EFOpenDataRepository();
+        }
+
+        public List<OpenData> FindOpenDataFromDb(string name)
+        {
+            return _efRepository.SelectAll(name);
+        }
+    }
+}

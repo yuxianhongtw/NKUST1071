99f7f4722f8071e9bafa4af108e84e044cd492e6
鈺弦 洪 <yuxianhongtw@gmail.com>
2018-11-16 19:29:51 +0800

    add 1109's test

---
+++ 1109/OpenDataImport/OpenDataImport/Program.cs
@@ -0,0 +1,43 @@
+﻿using OpenDataImport.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace OpenDataImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Service.ImportService importService = new Service.ImportService();
+            Service.EFImportService importService = new Service.EFImportService();
+            var nodes = importService.FindOpenDataFromDb("公共資訊");//importService.FindOpenDataFromXml();
+            //importService.ImportToDb(nodes);
+            showOpenData(nodes);
+            Console.ReadKey();
+
+        }
+        public void Configure()
+        {
+            string baseDir = Directory.GetCurrentDirectory();
+
+            AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Path.Combine(baseDir, "App_Data"));
+        }
+
+        private static void showOpenData(List<OpenData> nodes)
+        {
+
+            Console.WriteLine(string.Format("共收到{0}筆的資料", nodes.Count));
+            nodes.GroupBy(node => node.服務分類).ToList()
+                .ForEach(group =>
+                {
+                    var key = group.Key;
+                    var groupDatas = group.ToList();
+                    var message = $"服務分類:{key},共有{groupDatas.Count()}筆資料";
+                    Console.WriteLine(message);
+                });
+        }
+    }
+}
---
+++ 1109/OpenDataImport/OpenDataImport/Repository/EFOpenDataRepository.cs
@@ -0,0 +1,64 @@
+﻿using OpenDataImport.Database;
+using OpenDataImport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenDataImport.Repository
+{
+    class EFOpenDataRepository
+    {
+        private OpenDataDbContext OpenDataDbContext { get; set; }
+
+        public EFOpenDataRepository()
+        {
+            OpenDataDbContext = new OpenDataDbContext();
+        }
+
+        public List<OpenData> SelectAll(string name)
+        {
+            var result = new List<OpenData>();
+            var query = OpenDataDbContext.OpenData.AsQueryable();
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(x => x.服務分類 == name);
+            }
+            //Orderby(query);
+            return query.ToList();
+        }
+
+        /*private void Orderby(IQueryable<OpenData> query)
+        {
+            query.OrderBy(x => x.資料集名稱);
+        }*/
+
+        public void Insert(OpenData item)
+        {
+            OpenDataDbContext.OpenData.Add(item);
+            OpenDataDbContext.SaveChanges();
+        }
+
+        public void Update(OpenData item)
+        {
+            var exist = OpenDataDbContext.OpenData
+                .Where(x => x.id == item.id).SingleOrDefault();
+            if (exist == null)
+                return;
+            exist.主要欄位說明 = item.主要欄位說明;
+            exist.服務分類 = item.服務分類;
+            exist.服務分類 = item.資料集名稱;
+            OpenDataDbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var exist = OpenDataDbContext.OpenData
+                .Where(x => x.id == id).SingleOrDefault();
+            if (exist == null)
+                return;
+            OpenDataDbContext.OpenData.Remove(exist);
+            OpenDataDbContext.SaveChanges();
+        }
+    }
+}

df338ea93dbd49daab8af8d4bf35d36bfb39dc33
鈺弦 洪 <yuxianhongtw@gmail.com>
2018-11-23 11:57:02 +0800

    update 1123's hw

---
+++ 1116/OpenDataImport/YX.Web/Controllers/OpenDataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using YX.Core.Database;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace YX.Web.Controllers
+{
+    public class OpenDataController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            OpenDataDbContext db = new OpenDataDbContext();
+            List<YX.Core.Models.OpenData> models = db.OpenData.ToList();
+
+            return View(models);
+        }
+    }
+}
